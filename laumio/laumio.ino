428e93917f8f7919fc3d1aa10ca1986eae9b0715
JbLb <jerome@jblb.net>
2019-01-07 22:59:20 +0100

    add OTA

--- laumio/laumio.ino
+++ laumio/laumio.ino
@@ -28,6 +28,7 @@
 #include <WiFiUdp.h>
 #include <Adafruit_NeoPixel.h>
 #include <EEPROM.h>
+#include <ArduinoOTA.h>
 /* Perhaps other later */
 
 #include "LaumioConnect.h"
@@ -67,6 +68,41 @@ void setup()
     Serial.println(hostString);
 
     conn.setHostname(hostString);
+
+  ArduinoOTA.setHostname(hostString);
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+
 }
 
 enum State { off, start, wifi_sta_connecting, wifi_sta_connected,
@@ -78,6 +114,8 @@ State laumio_previous_state = off;
 
 void loop()
 {
+ArduinoOTA.handle();
+
     // Changement d'état
     if (laumio_previous_state != laumio_state) {
         laumio_previous_state = laumio_state;
@@ -127,6 +165,7 @@ void loop()
         udpRC.begin();
         MDNS.begin(hostString);
         mqtt_client.begin();
+        ArduinoOTA.begin();
         laumio_state = ready;
         break;
     case wifi_sta_abort:
